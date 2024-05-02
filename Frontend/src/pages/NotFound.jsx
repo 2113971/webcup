8f59779f0312d27e974e40ff9bd6f10624064b2c
Luqman <luqmanthupsee@gmail.com>
2024-05-02 18:28:15 +0400

    Luqman - Frontend done

---
+++ Frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The Page you're Looking For Does Not Exist!</p>
+    </div>
+  )
+}
+
+export default NotFound
